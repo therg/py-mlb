715b602842d287c2e4c09e9a370055778f966530
Jason Bachman <jason.bachman@gmail.com>
2012-06-13 15:34:58 -0400

    Added query method to db

--- py_mlb/db.py
+++ py_mlb/db.py
@@ -6,73 +6,86 @@ import ConfigParser, os
 from py_mlb import logger
 
 class DB:
-	"""Super dumb wrapper around a MySQLdb connection object"""
-	def __init__(self):
-		config = ConfigParser.ConfigParser()
-		config.read(['.db.cfg', os.path.expanduser('~/.db.cfg')])
+    """Super dumb wrapper around a MySQLdb connection object"""
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read(['.db.cfg', os.path.expanduser('~/.db.cfg')])
 
-		simplefilter("error", MySQLdb.Warning)
+        simplefilter("error", MySQLdb.Warning)
 
-		if config.has_section('db') \
-		and config.has_option('db', 'db'):
-			c = {}
-			for key, value in config._sections['db'].iteritems():
-				if not key.startswith('__'):
-					c[key] = value
+        if config.has_section('db') \
+        and config.has_option('db', 'db'):
+            c = {}
+            for key, value in config._sections['db'].iteritems():
+                if not key.startswith('__'):
+                    c[key] = value
 
-			try:
-				self.db = MySQLdb.connect(**c)
-			except MySQLdb.Error, e:
-				raise
-
-
-	@property
-	def rowcount(self):
-		"""
-		Returns the number of rows affected/returned by the last query
-		"""
-		return self._count
+            try:
+                self.db = MySQLdb.connect(**c)
+                self.cursor = self.db.cursor()
+            except MySQLdb.Error, e:
+                raise
 
 
-	def savedict(self, obj, table):
-		"""
-		Persists a dict to a specified table, assumes all keys in dict are
-		valid columns in table
-		"""
-		if not isinstance(obj, dict): return False
+    @property
+    def rowcount(self):
+        """
+        Returns the number of rows affected/returned by the last query
+        """
+        return self._count
 
-		keys = ['`%s`' % key for key in obj.keys()]
-		values = [None if value == '' else value for value in obj.values()]
+    def savedict(self, obj, table):
+        """
+        Persists a dict to a specified table, assumes all keys in dict are
+        valid columns in table
+        """
+        if not isinstance(obj, dict): return False
 
-		sql = 'REPLACE INTO %s (%s) VALUES (%s)' % (table, ','.join(keys), ','.join(['%s'] * len(values)))
-		self.execute(sql, values)
+        keys = ['`%s`' % key for key in obj.keys()]
+        values = [None if value == '' else value for value in obj.values()]
+
+        sql = 'REPLACE INTO %s (%s) VALUES (%s)' % (table, ','.join(keys), ','.join(['%s'] * len(values)))
+        self.execute(sql, values)
+
+    def query(self, columns, table, matchColumn=None, matchValue=None):
+        """
+        Executes a search query
+        SELECT [<columns>] FROM <table> WHERE <matchColumn>='<matchValue>';
+        returns a tuple of results
+        """
+        if matchValue and matchColumn:
+            sql = """SELECT %s FROM %s WHERE %s='%s'""" % (','.join(columns), table, matchColumn, matchValue)
+            print "SQL Statement: " + sql
+        else:
+            sql = """SELECT %s FROM %s""" % (','.join(columns))
+
+        self.db.query(sql)
+        queryResult = self.db.store_result()
+        return queryResult.fetch_row(maxrows=0)
+
+    def execute(self, sql, values = None):
+        """
+        Executes a query
+        
+        Arguments:
+        sql - The query
+        values - A list of values to be bound
+        """
+        self._count = None
+
+        try:
+            self.cursor.execute(sql, values)
+            self.db.commit()
+        except (MySQLdb.Warning, MySQLdb.Error), e:
+            logger.error('QUERY ERROR: %s\nQUERY: %s\nVALUES: %s\n\n' % (e, sql, ','.join([str(v) for v in values])))
+            pass
+
+        self._count = self.cursor.rowcount
+        self.cursor.close()
 
 
-	def execute(self, sql, values = None):
-		"""
-		Executes a query
-		
-		Arguments:
-		sql - The query
-		values - A list of values to be bound
-		"""
-		self._count = None
-
-		cursor = self.db.cursor()
-
-		try:
-			cursor.execute(sql, values)
-			self.db.commit()
-		except (MySQLdb.Warning, MySQLdb.Error), e:
-			logger.error('QUERY ERROR: %s\nQUERY: %s\nVALUES: %s\n\n' % (e, sql, ','.join([str(v) for v in values])))
-			pass
-
-		self._count = cursor.rowcount
-		cursor.close()
-
-
-	def close(self):
-		"""
-		Closes the database connection
-		"""
-		self.db.close()
+    def close(self):
+        """
+        Closes the database connection
+        """
+        self.db.close()
--- py_mlb/schedule.py
+++ py_mlb/schedule.py
@@ -8,77 +8,78 @@ import re
 from . import parseJSON
 
 class Schedule(dict):
-	"""The schedule for a given day"""
-	def __init__(self, year, month, day = None):
-		"""
-		Constructor
-		
-		Arguments:
-		year: The... year!
-		month: The... month!
-		day: The... day! (or None for all days of the month)
-		
-		Schedule is a standard dictionary: each day is a key in the format of 'YYYY-MM-DD', each value
-		a list of game dictionaries.
-		"""
-		days = []
-		
-		if day is None:
-			for d in xrange(1, calendar.mdays[month] + 1):
-				days.append(datetime.date(year, month, d))
-		else:
-			days.append(datetime.date(year, month, day))
+    """The schedule for a given day"""
+    def __init__(self, year, month, day = None):
+        """
+        Constructor
+        
+        Arguments:
+        year: The... year!
+        month: The... month!
+        day: The... day! (or None for all days of the month)
+        
+        Schedule is a standard dictionary: each day is a key in the format of 'YYYY-MM-DD', each value
+        a list of game dictionaries.
+        """
+        days = []
+        
+        if day is None:
+            for d in xrange(1, calendar.mdays[month] + 1):
+                days.append(datetime.date(year, month, d))
+        else:
+            days.append(datetime.date(year, month, day))
 
-		for d in days:
-			key = d.strftime("%Y-%m-%d")
-			if key not in self.keys():
-				self[key] = []
+        for d in days:
+            key = d.strftime("%Y-%m-%d")
+            if key not in self.keys():
+                self[key] = []
 
-			f = Fetcher(Fetcher.MLB_SCHEDULE_URL, date=d.strftime("%Y%m%d"))
-			try:
-			 	content = f.fetch(True)
-				if len(content) == 0: continue
-				content = re.sub(r'\t+', '\t', content)
-				content = content.replace('"', '\\"')
-				content = content.replace("'", "\"")
-				content = re.sub(r'\t([\w,_]+):\s', r'"\1":', content)
-				obj = json.loads(content)
-				self[key] = obj
-			except ValueError, e:
-				print "ERROR %s on %s" % (e, f.url)
-				pass
-	
-	def save(self):
-		try:
-			db = DB()
-		except:
-			return False
+            f = Fetcher(Fetcher.MLB_SCHEDULE_URL, date=d.strftime("%Y%m%d"))
+            try:
+                content = f.fetch(True)
+                if len(content) == 0:
+                    continue
+                content = re.sub(r'\t+', '\t', content)
+                content = content.replace('"', '\\"')
+                content = content.replace("'", "\"")
+                content = re.sub(r'\t([\w,_]+):\s', r'"\1":', content)
+                obj = json.loads(content)
+                self[key] = obj
+            except ValueError, e:
+                print "ERROR %s on %s" % (e, f.url)
+                pass
+    
+    def save(self):
+        try:
+            db = DB()
+        except:
+            return False
 
-		for day, games in self.iteritems():
-			for game in games:
-				
-				game['home_team_id'] = game['home']['id']
-				game['home_team_full'] = game['home']['full']
-				game['home_team_display_code'] = game['home']['display_code']
-				game['home_probable_id'] = game['home']['probable_id']
-				game['away_team_id'] = game['away']['id']
-				game['away_team_full'] = game['away']['full']
-				game['away_team_display_code'] = game['away']['display_code']
-				game['away_probable_id'] = game['away']['probable_id']
-				game['game_date'] = game['game_id'][0:10].replace('/', '-')
-				
-				if game['game_status'] == 'F':
-					game['home_score'] = game['home']['result']
-					game['away_score'] = game['away']['result']
-				else:
-					game['home_score'] = None
-					game['away_score'] = None
+        for day, games in self.iteritems():
+            for game in games:
+                
+                game['home_team_id'] = game['home']['id']
+                game['home_team_full'] = game['home']['full']
+                game['home_team_display_code'] = game['home']['display_code']
+                game['home_probable_id'] = game['home']['probable_id']
+                game['away_team_id'] = game['away']['id']
+                game['away_team_full'] = game['away']['full']
+                game['away_team_display_code'] = game['away']['display_code']
+                game['away_probable_id'] = game['away']['probable_id']
+                game['game_date'] = game['game_id'][0:10].replace('/', '-')
+                
+                if game['game_status'] == 'F':
+                    game['home_score'] = game['home']['result']
+                    game['away_score'] = game['away']['result']
+                else:
+                    game['home_score'] = None
+                    game['away_score'] = None
 
-				del(game['home'])
-				del(game['away'])
-				del(game['pitcher'])
-				
-				sql = 'REPLACE INTO schedule (%s) VALUES (%s)' % (','.join(game.keys()), ','.join(['%s'] * len(game.values())))
-				db.execute(sql, game.values())
-			
-		db.close()
+                del(game['home'])
+                del(game['away'])
+                del(game['pitcher'])
+                
+                sql = 'REPLACE INTO schedule (%s) VALUES (%s)' % (','.join(game.keys()), ','.join(['%s'] * len(game.values())))
+                db.execute(sql, game.values())
+            
+        db.close()
